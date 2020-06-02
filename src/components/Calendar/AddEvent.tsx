c1f531c1b4e0cfd29ef23dcf4b9779f54a86bd26
Alek Angelov <alekangelov@Aleks-MacBook-Pro-2.local>
2020-06-02 07:42:01 +0200

    adding types

--- src/components/Calendar/AddEvent.tsx
+++ src/components/Calendar/AddEvent.tsx
@@ -1,12 +1,27 @@
 import React, { Props, FunctionComponent } from "react";
-
+import { useSpring, a } from "react-spring";
 type AddEventProps = {
   active: boolean;
 };
 
-const AddEvent: FunctionComponent<AddEventProps> = (props) => {
-  console.log(props.active);
-  return <div className="add-event"></div>;
+const AddEvent: FunctionComponent<AddEventProps> = ({ active }) => {
+  const style = useSpring({
+    display: active ? "block" : "none",
+    opacity: active ? 1 : 0,
+    transform: active ? "translate(-50%,-50%)" : "translate(-50%,-100%)",
+  });
+  return (
+    <a.div style={style} className="add-event">
+      <form className="form">
+        <div className="form-group">
+          <input type="text" placeholder="title"></input>
+        </div>
+        <div className="form-group">
+          <textarea>Description</textarea>
+        </div>
+      </form>
+    </a.div>
+  );
 };
 
 export default AddEvent;
